69745facd908587cdfac8cb72802627f64235c23
Joachim Schrøder <joachimandersson0710@gmail.com>
2023-05-18 14:13:20 +0200

    black + isort

--- mapd/visualization/visualization_tool.py
+++ mapd/visualization/visualization_tool.py
@@ -1,13 +1,15 @@
 import os
-import pyarrow as pa
-import pyarrow.parquet as pq
-import pyarrow.dataset as ds
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import random
-from typing import List, Dict, Tuple, Union, Optional
+from typing import Dict, List, Optional, Tuple, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pyarrow.dataset as ds
+import pyarrow.parquet as pq
+import seaborn as sns
+
 from mapd.probes.probe_suite_generator import ProbeSuiteDataset
 
 
@@ -27,88 +29,16 @@ class MAPDVisualizationTool:
         )
 
         self.probe_suite_dataset = probe_suite_dataset
-        self.suite_indices = self.probe_suite_dataset.index_to_suite
+        self._suite_indices = self.probe_suite_dataset.index_to_suite
 
-        self.val_data = (
-            self.data.filter(ds.field("stage") == "val").to_table().to_pandas()
-        )
-        self.val_data["epoch"] = self.val_data["epoch"].astype(int)
-        self.val_indices = self.val_data["sample_index"].unique()
-
-        self.train_data = (
-            self.data.filter(ds.field("stage") == "train").to_table().to_pandas()
-        )
-        self.train_data = self.train_data[
-            ~self.train_data["sample_index"].isin(self.val_indices)
-        ]
-        self.train_data["epoch"] = self.train_data["epoch"].astype(int)
-
-        # add probe suite to train called "Train"
-        self.train_data["suite"] = "Other"
-
-        self.suite_to_name = self._suite_to_name()
-
-        for suite_attr, suite_name in self.suite_to_name.items():
-            indices = [
-                idx for idx, suite in self.suite_indices.items() if suite == suite_attr
-            ]
-            random.shuffle(indices)
-            train_indices = indices[:250]
-            val_indices = indices[250:]
-            self.val_data.loc[
-                self.val_data["sample_index"].isin(train_indices), "suite"
-            ] = suite_name
-            self.val_data.loc[
-                self.val_data["sample_index"].isin(val_indices), "suite"
-            ] = (suite_name + " [Val]")
-
-        self.val_data["prediction"] = self.val_data["y"] == self.val_data["y_hat"]
-        self.val_data = self.val_data.groupby(["epoch", "suite"]).agg(
-            {"prediction": "mean"}
-        )
-        self.val_data.reset_index(inplace=True)
-        self.val_data["prediction"] = self.val_data["prediction"] * 100
-
-        self.train_data["prediction"] = self.train_data["y"] == self.train_data["y_hat"]
-        self.train_data = self.train_data.groupby(["epoch", "suite"]).agg(
-            {"prediction": "mean"}
-        )
-        self.train_data.reset_index(inplace=True)
-        self.train_data["prediction"] = self.train_data["prediction"] * 100
+        self._prepare_val_data()
+        self._prepare_train_data()
 
         # all suites
-        self._val_suites = self.val_data["suite"].unique()
-        self._train_suites = self.train_data["suite"].unique()
         self.all_suites = sorted(self._val_suites) + sorted(self._train_suites)
 
-        self._line_styles = ["solid", "dashed", "dashdot", "dotted"]
-
-        self._marker_list = ["o", "X"]
-
-        self._marker_colors = [
-            "tab:red",
-            "tab:blue",
-            "tab:purple",
-            "tab:orange",
-            "tab:green",
-            "tab:pink",
-            "tab:olive",
-            "tab:brown",
-            "tab:cyan",
-        ]
-
-    def _suite_to_name(self) -> Dict[str, str]:
-        self.suites = list(set(self.suite_indices.values()))
-        temp_dict = {}
-        for suite in self.suites:
-            test = suite.split("_")
-
-            # capitalize first word of each suite
-            test = [word.capitalize() for word in test]
-            # join words with space
-            test = " ".join(test)
-            temp_dict[suite] = test
-        return temp_dict
+        # prepare plot styles
+        self._prepare_plot_styles()
 
     def probe_accuracy_plot(
         self,
@@ -216,3 +146,83 @@ class MAPDVisualizationTool:
                 save is True
             ), "Remember to set save to True if you want to save the plot!"
         pass
+
+    def _suite_to_name(self) -> Dict[str, str]:
+        self.suites = list(set(self._suite_indices.values()))
+        temp_dict = {}
+        for suite in self.suites:
+            test = suite.split("_")
+
+            # capitalize first word of each suite
+            test = [word.capitalize() for word in test]
+            # join words with space
+            test = " ".join(test)
+            temp_dict[suite] = test
+        return temp_dict
+
+    def _prepare_val_data(self):
+        self.val_data = (
+            self.data.filter(ds.field("stage") == "val").to_table().to_pandas()
+        )
+        self.val_data["epoch"] = self.val_data["epoch"].astype(int)
+        self.val_indices = self.val_data["sample_index"].unique()
+
+        self.suite_to_name = self._suite_to_name()
+
+        for suite_attr, suite_name in self.suite_to_name.items():
+            indices = [
+                idx for idx, suite in self._suite_indices.items() if suite == suite_attr
+            ]
+            random.shuffle(indices)
+            train_indices = indices[:250]
+            val_indices = indices[250:]
+            self.val_data.loc[
+                self.val_data["sample_index"].isin(train_indices), "suite"
+            ] = suite_name
+            self.val_data.loc[
+                self.val_data["sample_index"].isin(val_indices), "suite"
+            ] = (suite_name + " [Val]")
+
+        self.val_data["prediction"] = self.val_data["y"] == self.val_data["y_hat"]
+        self.val_data = self.val_data.groupby(["epoch", "suite"]).agg(
+            {"prediction": "mean"}
+        )
+        self.val_data.reset_index(inplace=True)
+        self.val_data["prediction"] = self.val_data["prediction"] * 100
+        self._val_suites = self.val_data["suite"].unique()
+
+    def _prepare_train_data(self):
+        self.train_data = (
+            self.data.filter(ds.field("stage") == "train").to_table().to_pandas()
+        )
+        self.train_data = self.train_data[
+            ~self.train_data["sample_index"].isin(self.val_indices)
+        ]
+        self.train_data["epoch"] = self.train_data["epoch"].astype(int)
+
+        # add probe suite to train called "Train"
+        self.train_data["suite"] = "Other"
+        self.train_data["prediction"] = self.train_data["y"] == self.train_data["y_hat"]
+        self.train_data = self.train_data.groupby(["epoch", "suite"]).agg(
+            {"prediction": "mean"}
+        )
+        self.train_data.reset_index(inplace=True)
+        self.train_data["prediction"] = self.train_data["prediction"] * 100
+        self._train_suites = self.train_data["suite"].unique()
+
+    def _prepare_plot_styles(self):
+        self._line_styles = ["solid", "dashed", "dashdot", "dotted"]
+
+        self._marker_list = ["o", "X"]
+
+        self._marker_colors = [
+            "tab:red",
+            "tab:blue",
+            "tab:purple",
+            "tab:orange",
+            "tab:green",
+            "tab:pink",
+            "tab:olive",
+            "tab:brown",
+            "tab:cyan",
+        ]
